72039aea8f9bfa74e316e817f42c204fad2769a9
Matt Briggs <matt@mattbriggs.net>
2014-08-21 09:10:22 -0400

    expand path

--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -2,13 +2,13 @@ return if File.directory?(File.expand_path '~/.rbenv')
 
 git 'checkout rbenv' do
   repository 'https://github.com/sstephenson/rbenv.git'
-  destination '~/.rbenv'
+  destination File.expand_path('~/.rbenv')
 end
 
 node['mbriggs']['rbenv']['plugins'].each do |plugin|
   git "install #{plugin['name']}" do
     repository "http://github.com/#{plugin['gh']}.git"
-    destination "~/.rbenv/plugins/#{plugin['name']}"
+    destination File.expand_path("~/.rbenv/plugins/#{plugin['name']}")
   end
 end
 
