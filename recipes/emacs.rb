298c132bdce8de14d26793ba8cd895203e011ed5
Matt Briggs <matt@mattbriggs.net>
2014-08-21 23:31:10 -0400

    install emacs better

--- recipes/emacs.rb
+++ recipes/emacs.rb
@@ -1,5 +1,9 @@
-git "~/.emacs.d" do
-  repository "https://github.com/mbriggs/.emacs.d"
+unless File.directory?('/usr/local/Cellar/emacs')
+  command('install emacs') { 'brew install emacs --cocoa' }
+end
+
+git '~/.emacs.d' do
+  repository 'https://github.com/mbriggs/.emacs.d'
   destination File.expand_path('~/.emacs.d')
   action :sync
 end
