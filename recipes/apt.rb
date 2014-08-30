b141ad4ca79c54f79beb1f188ab645e7ab425601
Matt Briggs <matt@mattbriggs.net>
2014-08-30 03:03:22 -0400

    fix

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -3,5 +3,5 @@ execute "update" do
 end
 
 execute "install apt packages" do
-  command "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y #{node['mbriggs']['apt'].join ' '}"
+  command "sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' -f -q install -y #{node['mbriggs']['apt'].join ' '}"
 end
