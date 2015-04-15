63b7069d9c6a5f4cce835cd6b789e54f739d81b6
Matt Briggs <matt@mattbriggs.net>
2015-04-14 21:17:55 -0400

    add a link

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -9,8 +9,13 @@ git "~/.zsh" do
 end
 
 execute "install zsh" do
-  cwd root
-  command "make install"
+  cwd root command "make install"
+  user node['mbriggs']['user']
+  group node['mbriggs']['group']
+end
+
+link "#{root}/zshrc" do
+  to "/home/#{node['mbriggs']['user']}/.zshrc"
   user node['mbriggs']['user']
   group node['mbriggs']['group']
 end
